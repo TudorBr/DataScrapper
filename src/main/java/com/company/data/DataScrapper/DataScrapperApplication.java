091832c8104859e6c0f433d1202ec77538d0583f
Tudor <tudor.bragaru@gmail.com>
2025-07-19 21:39:19 +0300

    repository

---
+++ src/main/java/com/company/data/DataScrapper/DataScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.company.data.DataScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScrapperApplication.class, args);
+	}
+
+}
